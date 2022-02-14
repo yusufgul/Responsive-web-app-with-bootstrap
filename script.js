12557276ad14a1497c459236ca6159d213d4a76e
Yusuf <yu.suf.gul@gmail.com>
2022-02-14 21:05:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function myFunction() { //pop-up
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
